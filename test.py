0ed638adbe1b8947562ca27e72f46b574207c508
Christian Bueno <chmabuen@espol.edu.ec>
2024-09-10 17:52:32 -0500

    refactor: edit test

--- test.py
+++ test.py
@@ -8,3 +8,30 @@ delta15min = timedelta(minutes=15)
 expire = now + delta15min
 print(f"now: {now}\ndelta15min: {delta15min}\nexpire: {expire}")
 # print(f"now2: {now2}")
+
+#
+import logging
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    getenv("FRONTEND_URL"),
+]
+
+# AttributeError: module 'bcrypt' has no attribute '__about__'
+logging.getLogger('passlib').setLevel(logging.ERROR)
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
